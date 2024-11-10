df2218d298fdea4f318075c2513dfba7b46c7fcc
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2024-11-09 23:21:01 -0300

    initial

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -17,7 +17,7 @@ function Page() {
   return (
     <div className="md:w-9/12 bg-white py-2 px-4  m-auto mt-1">
       <fieldset className=" border border-dashed border-separate border-red900">
-        <legend className=" px-4 mx-6 rounded">
+        <legend className=" py-2 px-4 mx-6 rounded">
           <div className="flex gap-x-4 info-title rounded">
             <Icon
               src="colegiate"
